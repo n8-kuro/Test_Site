efe8682219a3a81a5cb2448e0dc38e48bd7d4aa3
Ruslan Baimagambetov <vkassandrarvv13@gmail.com>
2026-05-02 22:52:31 +0600

    poject init

---
+++ savepoint/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">Savepoint Archive</h1>
+      <p className="mt-4">Здесь будет моя библиотека игр и фильмов.</p>
+    </main>
+  );
+}
